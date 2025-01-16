bad285f02f1eea0838b2b5bd9e58d27e4cdde57a
Yemmewarrahul <rahul.yemmewar>
2025-01-16 22:17:22 +0530

    created list of array to generate buttons

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -3,11 +3,25 @@ import React, { useState } from "react";
 const CalculatorWithJestTestCases = () => {
   const [input, setInput] = useState("");
 
+  const arrayButtons = [
+    "7", "8", "9", "/", 
+    "4", "5", "6", "*", 
+    "1", "2", "3", "-", 
+    "0", ".", "C", "+", 
+    "="
+];
+
   return (
     <>
       <div className="calculator">
-        <div className="display">{"0"}</div>
-        
+        <div className="display">{input ||"0"}</div>
+        <div className="buttons">
+                {arrayButtons.map((item, i) => (
+                    <div>
+                        {item}
+                    </div>
+                ))}
+            </div>
       </div>
     </>
   );
